c332152b7caf7d5b42e72c029fc6dc4b6981c916
vale <vale@debian.sin>
2020-07-23 14:01:33 +0200

    LISTARUSUARIOSJSON COMPLETO

--- WebContent/menu.js
+++ WebContent/menu.js
@@ -12,7 +12,7 @@ const URL_LISTAR_USUARIOS_JSON = "/loginjee/ListarUsuariosJSON"
 //SERVICIO ES SINÓNIMO DE Servlet en JAVA
 var xhr = new XMLHttpRequest();
 function listarUsuariosServidor() {
-	alert("Entrando en listarUsuariosServidor");
+	//alert("Entrando en listarUsuariosServidor");
 	xhr.open("GET", URL_LISTAR_USUARIOS_JSON);
 	//deberíamos setear el content-type
 	xhr.onreadystatechange = respuestaListaUsuariosJson;//programar el callback
@@ -21,13 +21,55 @@ function listarUsuariosServidor() {
 
 }
 
+//{"id":1,"nombre":"vale","pwd":"valeri"}
+function crearTRUsuario (usuario)
+{
+	let fila_usuario = document.createElement ("tr");
+	let columna_id = document.createElement ("td");
+	let columna_nombre = document.createElement ("td");
+	let columna_pwd = document.createElement ("td");
+	
+	columna_id.innerHTML = usuario.id;
+	columna_nombre.innerHTML = usuario.nombre;
+	columna_pwd.innerHTML = usuario.pwd;
+	
+	fila_usuario.appendChild (columna_id);
+	fila_usuario.appendChild (columna_nombre);
+	fila_usuario.appendChild (columna_pwd);
+	
+	console.log ("ID USUARIO = " + usuario.id);
+	console.log ("NOMBRE USUARIO = " + usuario.nombre);
+	console.log ("ID USUARIO = " + usuario.pwd);
+	
+	return fila_usuario;
+}
+
+function mostrarUsuarios (listausuarios)
+{
+	let lu = JSON.parse(listausuarios);//de texto a variable js
+	
+	let tabla_usuarios = document.createElement ("table");
+	let div_usuarios = document.getElementById ("divusuuarios");
+	div_usuarios.appendChild (tabla_usuarios);
+	let fila_nueva;
+	
+	for (let i = 0; i < lu.length; i++)
+		{
+			fila_nueva = crearTRUsuario (lu[i]);
+			tabla_usuarios.appendChild (fila_nueva);
+		}
+	
+}
+
 function respuestaListaUsuariosJson() {
 	//esta función será invocada cuando vuelva del servidor
 	if (xhr.readyState == 4) {
 		console.log("La respuesta del servidor ha llegado");
 		if (xhr.status == 200) {
 			alert("Lista de usuarios recibida");
+			//TODO mostrar la lista en la paǵina
 			console.log (xhr.responseText);
+			mostrarUsuarios (xhr.responseText);
 		} else if (xhr.status == 204) {
 			alert("La lista está vacía");
 			}
