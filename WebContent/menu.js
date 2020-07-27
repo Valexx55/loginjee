a5be00e308e1292bbe3aa9580e58cafb0f3ba6ed
vale <vale@debian.sin>
2020-07-27 14:06:49 +0200

    JSP JSTL EL LISTADO USUARIOS

--- WebContent/menu.js
+++ WebContent/menu.js
@@ -7,6 +7,7 @@
 //Y ENVIARLOS AL SERVIDOR, PARA COMPROBAR SI ESE USUARIO EXISTE
 //O ESTÁ REGISTRADO
 const URL_LISTAR_USUARIOS_JSON = "/loginjee/ListarUsuariosJSON"
+const URL_LISTAR_USUARIO_JSON = "/loginjee/ListarUsuarioJSON"
 
 //https://www.bing.com/search?q=realm+adrid&cvid=6da69e504f24467c997e6a1210719880&FORM=ANNTA1&PC=U531
 //SERVICIO ES SINÓNIMO DE Servlet en JAVA
@@ -26,6 +27,13 @@ function obtenerDetalle ()
 	//usar el this
 	console.log ("Ha tocado el id = " + this.id);
 	//TODO 1.ir al servidor y pedirle la info de un usuario dándole el id
+	
+	let url_previa = URL_LISTAR_USUARIO_JSON+"?id="+this.id;
+	console.log (url_previa);
+	xhr.open("GET", url_previa);
+	//deberíamos setear el content-type
+	xhr.onreadystatechange = respuestaListaUsuarioJson;//programar el callback
+	xhr.send(null);
 	//2. recibir el json del usuario del servidor y mostrar su detalle
 }
 
@@ -73,6 +81,17 @@ function mostrarUsuarios (listausuarios)
 	
 }
 
+function mostrarUsuario (usuariojson)
+{
+	let u = JSON.parse(usuariojson);//de texto a variable js
+	
+	//let tabla_usuarios = document.createElement ("table");
+	let div_usuarios = document.getElementById ("detalleusuario");
+	let string_info_usuario = "ID = " + u.id + " PWD = "+ u.pwd;
+	div_usuarios.innerHTML = string_info_usuario;
+	
+}
+
 function respuestaListaUsuariosJson() {
 	//esta función será invocada cuando vuelva del servidor
 	if (xhr.readyState == 4) {
@@ -84,6 +103,25 @@ function respuestaListaUsuariosJson() {
 			mostrarUsuarios (xhr.responseText);
 		} else if (xhr.status == 204) {
 			alert("La lista está vacía");
+			}
+		else if (xhr.status == 500) {
+			alert("Ha habido un error. Intentelo más tarde");
+		}
+	}
+
+}
+
+function respuestaListaUsuarioJson() {
+	//esta función será invocada cuando vuelva del servidor
+	if (xhr.readyState == 4) {
+		console.log("La respuesta del servidor ha llegado");
+		if (xhr.status == 200) {
+			alert("Info Usuario recibida");
+			//TODO mostrar la lista en la paǵina
+			console.log (xhr.responseText);
+			mostrarUsuario (xhr.responseText);
+		} else if (xhr.status == 204) {
+			alert("No existe info de ese usuario");
 			}
 		else if (xhr.status == 500) {
 			alert("Ha habido un error. Intentelo más tarde");
---
+++ src/loginjee/controlador/ListarUsuariosJSP.java
@@ -0,0 +1,90 @@
+package loginjee.controlador;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+
+import loginjee.bean.Usuario;
+import loginjee.servicio.UsuarioService;
+
+/**
+ * Servlet implementation class ListarUsuariosJSON
+ */
+@WebServlet("/ListarUsuariosJSP")
+public class ListarUsuariosJSP extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private final static Logger log = Logger.getLogger("mylog");
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ListarUsuariosJSP() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		log.debug("LLAMANDO A PEDIR USUARIOS");
+		UsuarioService usuarioService = new UsuarioService();
+		List<Usuario> lu = null;
+		try {
+			lu = usuarioService.listarUsuarios();
+			//PASAR lu "beans" a UN JSP
+			request.setAttribute("lu", lu);
+			//request.getRequestDispatcher("listausuarios.jsp").forward(request, response);
+			request.getRequestDispatcher("listausuarios2.jsp").forward(request, response);
+			
+			/*
+			// PASAR A JSON LU.
+			if ((lu != null) && (lu.size() > 0)) //AND CORTOCIRCUito
+			{
+				log.debug("La lista trae usuarios " + lu);
+				Gson gson = new Gson();
+				String lu_json = gson.toJson(lu);// pasamos de lista
+				// MANDARLO EN BODY.
+				response.getWriter().append(lu_json);
+				// STATUS 200.
+				response.setStatus(HttpURLConnection.HTTP_OK);
+				// TIPO MIME application/json
+				response.setContentType("application/json");
+			} else {
+				response.setStatus(HttpURLConnection.HTTP_NO_CONTENT);
+			}*/
+			// STATUS 204 NO CONTENT si la cosa fue buen, pero la lista está vacía
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			log.error("Error recuperando la lista de usarios", e);
+			// STATUS 500;
+			response.setStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+	}
+
+}
