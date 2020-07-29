b08461dbcb4bcce26989daad2dd4a8b55bd91dec
vale <vale@debian.sin>
2020-07-29 14:00:54 +0200

    registro nuevo usuario

---
+++ WebContent/registro.js
@@ -0,0 +1,16 @@
+//EL NOMBRE DEL USUARIO TIENE QUE SER MINIMO DE 2
+//LA PWD DEL USUARIO TIENE QUE SER DE AL MENOS 2
+let nombre_correcto = false;
+let pwd_correcto = false;
+let formulario_correcto = false;
+//TODO validar formulario 
+
+function modificadoNombre() 
+{
+	console.log ("ha introducido algo en nombre");
+	}
+
+function modificadoPwd()
+{
+	console.log ("ha introducido algo en pwd");
+	}
