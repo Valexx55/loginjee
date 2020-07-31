aac719c5b83f16f6f78c5440021ee7a11eb488c1
vale <vale@debian.sin>
2020-07-31 13:57:15 +0200

    comprobamos la disponiblidad del nombre

--- WebContent/registro.js
+++ WebContent/registro.js
@@ -1,7 +1,44 @@
 //EL NOMBRE DEL USUARIO TIENE QUE SER MINIMO DE 2
 //LA PWD DEL USUARIO TIENE QUE SER DE AL MENOS 2
+//TODO controlar que cuando el nombre NO esté disponible, el botón esté deshabilitado
 
-//TODO validar formulario 
+const ULR_COMPROBAR_NOMBRE_DISPONIBLE = "/loginjee/ExisteNombre"
+var xhr = new XMLHttpRequest();
+
+function comprobarNombreDisponible() {
+	console.log ("Comprobando nombre ...");
+	let nuevo_nombre = document.getElementById("nombre").value;
+	if (nombreCorrecto (nuevo_nombre))
+		{
+		let url = ULR_COMPROBAR_NOMBRE_DISPONIBLE + "?nombre=" +nuevo_nombre;
+		console.log ("get sobre " + url);
+		//LLAMAR AL SERVIDOR
+		xhr.open("GET", url);
+		//deberíamos setear el content-type
+		xhr.onreadystatechange = respuestaNombreDisponible;//programar el callback
+		xhr.send(null);
+		}
+}
+function respuestaNombreDisponible ()
+{
+	if (xhr.readyState == 4) {
+		console.log("La respuesta del servidor ha llegado");
+		if (xhr.status == 200) {
+			//esta disponible
+			console.log ("disponible");
+			let elemento_error = document.getElementById("errornombre");
+			elemento_error.innerHTML = "";
+		} else if (xhr.status == 409) {
+			//NO esta disponible
+			console.log (" NO disponible");
+			let elemento_error = document.getElementById("errornombre");
+			elemento_error.innerHTML = "¡Nombre no disponible!";
+		} else if (xhr.status == 500) {
+			console.log ("ERROR");
+		}
+	}
+}
+
 function nombreCorrecto (nombre)
 {
 	let correcto = false;
--- src/loginjee/servicio/UsuarioService.java
+++ src/loginjee/servicio/UsuarioService.java
@@ -8,66 +8,88 @@ import loginjee.bean.Usuario;
 import loginjee.persistencia.UsuarioDAO;
 
 public class UsuarioService {
-	//TENDEREMOS QUE TENER TODOS LOS MÉTODOS RELATIVOS AL TRABAJO CON USUARIOS
-	//ESTA CLASE SERÁ INVOCADA POR EL SERVLET
-	//Y ESTA CLASE, INVOCARÁ A LA PERSISTENCIA (EN CASO DE SER NECESARIO)
-	
+	// TENDEREMOS QUE TENER TODOS LOS MÉTODOS RELATIVOS AL TRABAJO CON USUARIOS
+	// ESTA CLASE SERÁ INVOCADA POR EL SERVLET
+	// Y ESTA CLASE, INVOCARÁ A LA PERSISTENCIA (EN CASO DE SER NECESARIO)
+
 	private final static Logger log = Logger.getLogger("mylog");
-	
-	
-	public Usuario obtenerUsuario(int id_usuario) throws Exception
-	{
+
+	public Usuario obtenerUsuario(int id_usuario) throws Exception {
 		Usuario usuario = null;
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		
-			usuario = usuarioDAO.leerUsuarioBD(id_usuario);
-		
+
+		usuario = usuarioDAO.leerUsuarioBD(id_usuario);
+
 		return usuario;
 	}
-	
-	public List<Usuario> listarUsuarios () throws Exception
-	{
+
+	public List<Usuario> listarUsuarios() throws Exception {
 		List<Usuario> lUsuarios = null;
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		
-			lUsuarios = usuarioDAO.obtenerTodos();
-		
+
+		lUsuarios = usuarioDAO.obtenerTodos();
+
 		return lUsuarios;
 	}
-	
-	
+
+	public boolean nombreDisponible(String nombre) throws Exception {
+		boolean disponible = false;
+
+		List<Usuario> lu = null;
+
+		lu = this.listarUsuarios();//sería más optimo hacer una consulta por nombre en usuarioDAO
+		if (lu != null && lu.size() > 0) {
+			boolean encontrado = false;
+			int pos = 0;
+			Usuario usuario_aux = null;
+			do {
+				usuario_aux = lu.get(pos);
+				if (usuario_aux.getNombre().equals(nombre)) {
+					encontrado = true;
+				} else {
+					pos = pos + 1;
+				}
+
+			} while (!(encontrado) && (pos < lu.size()));
+			disponible = !encontrado;
+		} else {
+			disponible = true;
+		}
+
+		return disponible;
+	}
+
 	/**
 	 * Método que comprueba si el usuario recibido existe en el sistema
+	 * 
 	 * @param usuario el usuario que queremos comprobar
-	 * @return 0 si existe 1 si falla el pwd (pero existe el nombre) 2 no existe (no coincide ni nombre ni pwd)
+	 * @return 0 si existe 1 si falla el pwd (pero existe el nombre) 2 no existe (no
+	 *         coincide ni nombre ni pwd)
 	 * @throws Exception si hubo fallo con la base de datos
 	 */
-	
-	public int existeUsuario (Usuario usuario) throws Exception
-	{
+
+	public int existeUsuario(Usuario usuario) throws Exception {
 		int num_dev = -1;
 		UsuarioDAO usuarioDAO = null;
-			
-			log.debug("Entramos en servicio existeUsuario");
-		
-			usuarioDAO = new UsuarioDAO();
-			num_dev = usuarioDAO.existeUsuarioBD(usuario);
-			
-			log.debug("Salimos en servicio existeUsuario valor devuelto = " + num_dev);
-		
+
+		log.debug("Entramos en servicio existeUsuario");
+
+		usuarioDAO = new UsuarioDAO();
+		num_dev = usuarioDAO.existeUsuarioBD(usuario);
+
+		log.debug("Salimos en servicio existeUsuario valor devuelto = " + num_dev);
+
 		return num_dev;
-		
+
 	}
-	
-	public void altaUsuario (Usuario usuario) throws Exception
-	{
+
+	public void altaUsuario(Usuario usuario) throws Exception {
 		UsuarioDAO usuarioDAO = null;
-		
-			usuarioDAO = new UsuarioDAO();
-			usuarioDAO.altaUsuarioBD(usuario);
-			log.debug("Salimos del servicio altaUsuario ");
-			
-		
+
+		usuarioDAO = new UsuarioDAO();
+		usuarioDAO.altaUsuarioBD(usuario);
+		log.debug("Salimos del servicio altaUsuario ");
+
 	}
 
 }
